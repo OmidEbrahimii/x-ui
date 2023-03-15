be78f992ab5264aad955f2c98cfc9e75a186c64a
Omid Ebrahimi <omidsalari1381@gmail.com>
2023-03-16 01:08:27 +0330

    Update check_cpu_usage.go

--- web/job/check_cpu_usage.go
+++ web/job/check_cpu_usage.go
@@ -24,7 +24,7 @@ func (j *CheckCpuJob) Run() {
 	// get latest status of server
 	percent, err := cpu.Percent(1*time.Second, false)
 	if err == nil && percent[0] > float64(threshold) {
-		msg := fmt.Sprintf("🔴 وضعیت CPU : %.2f%% این مقدار بیشتر از مقدار تعیین شده است : %d%%", percent[0], threshold)
+		msg := fmt.Sprintf("<b>🔴 وضعیت سی پی یو : </b>%.2f%% \n<b>‼️ مقدار تعیین شده : </b>%d%% \n\n<b>⚠️ مقدار تعیین شده بیشتر از مقدار حال حاضر است لطفا پنل را چک کنید.</b>", percent[0], threshold)
 		j.tgbotService.SendMsgToTgbotAdmins(msg)
 	}
 }
